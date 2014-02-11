4484d365c76bf43959a677d695bbb6c7da306b3c
Kevin <kevin.melchert@gmail.com>
2014-02-11 13:48:20 +0100

    update js

--- placeholder_rotation.js
+++ placeholder_rotation.js
@@ -7,35 +7,33 @@ var PlaceholderRotation = function(el, options) {
   this.el.addEventListener('change', this.toggle.bind(this))
 }
 
-PlaceholderRotation.prototype = {
-  setup: function(data, opts) {
-    this.index    = data.index    || opts.index    || 0
-    this.delay    = data.delay    || opts.delay    || 1000
-    this.template = data.template || opts.template || "Search for {term}"
-    this.terms    = (data.terms && data.terms.split(/\s*,\s*/)) || opts.terms || ['something']
-  },
-  
-  toggle: function() {
-    return this.el.value=='' ? this.start() : this.stop()
-  },
-  
-  start: function() {
-    this.tick = setInterval(this.place.bind(this), this.delay)
-    return this
-  },
-  
-  stop: function() {
-    this.tick && clearInterval(this.tick)
-    return this
-  },
-  
-  place: function() {
-    this.el.setAttribute('placeholder', this.template.replace('{term}', this.term()))
-    return this
-  },
+PlaceholderRotation.prototype.setup = function(data, opts) {
+  this.index    = data.index    || opts.index    || 0
+  this.delay    = data.delay    || opts.delay    || 1000
+  this.template = data.template || opts.template || "Search for {term}"
+  this.terms    = (data.terms && data.terms.split(/\s*,\s*/)) || opts.terms || ['something']
+}
 
-  term: function() {
-    if (this.index >= this.terms.length) this.index = 0
-    return this.terms[this.index++]
-  }
+PlaceholderRotation.prototype.toggle = function() {
+  return this.el.value=='' ? this.start() : this.stop()
+}
+
+PlaceholderRotation.prototype.start = function() {
+  this.tick = setInterval(this.place.bind(this), this.delay)
+  return this
+}
+
+PlaceholderRotation.prototype.stop = function() {
+  this.tick && clearInterval(this.tick)
+  return this
+}
+
+PlaceholderRotation.prototype.place = function() {
+  this.el.setAttribute('placeholder', this.template.replace('{term}', this.term()))
+  return this
+}
+
+PlaceholderRotation.prototype.term = function() {
+  if (this.index >= this.terms.length) this.index = 0
+  return this.terms[this.index++]
 }
