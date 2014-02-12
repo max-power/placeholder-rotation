d79afb7fff2557dab2ebc539b92b0e240baaa45f
Kevin <kevin.melchert@gmail.com>
2014-02-12 10:00:06 +0100

    shorten code a bit

--- placeholder_rotation.js
+++ placeholder_rotation.js
@@ -2,8 +2,7 @@ var PlaceholderRotation = function(el, options) {
   if (el.tagName != 'INPUT' || !('placeholder' in el)) return false
   this.el = el
   this.setup(el.dataset, options || {})
-  this.place()
-  this.toggle()
+  this.place().toggle()
   this.el.addEventListener('change', this.toggle.bind(this))
 }
 
@@ -15,25 +14,21 @@ PlaceholderRotation.prototype.setup = function(data, opts) {
 }
 
 PlaceholderRotation.prototype.toggle = function() {
-  return this.el.value=='' ? this.start() : this.stop()
+  return this[''==this.el.value?'start':'stop']()
 }
 
 PlaceholderRotation.prototype.start = function() {
-  this.tick = setInterval(this.place.bind(this), this.delay)
+  this.tick||(this.tick = setInterval(this.place.bind(this), this.delay))
   return this
 }
 
 PlaceholderRotation.prototype.stop = function() {
-  this.tick && clearInterval(this.tick)
+  this.tick = clearInterval(this.tick)
   return this
 }
 
 PlaceholderRotation.prototype.place = function() {
-  this.el.setAttribute('placeholder', this.template.replace('{term}', this.term()))
+  this.el.setAttribute('placeholder', this.template.replace('{term}', this.terms[this.index]))
+  this.index=(this.index+1)%this.terms.length
   return this
-}
-
-PlaceholderRotation.prototype.term = function() {
-  if (this.index >= this.terms.length) this.index = 0
-  return this.terms[this.index++]
 }
